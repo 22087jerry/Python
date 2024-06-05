0370d5fcf6b9c8d0c2320695719b4ae294a2d10d
22087jerry <22087@burnside.school.nz>
2024-06-05 13:22:28 +1200

    second commit

--- game.py
+++ game.py
@@ -1,11 +1,27 @@
+#docstring- Jerry Xie- game_genere database application 
+#imports
 import sqlite3
 
+#contants and varibales
+DATABASE = "gamers.db"
 
-def print_all():
-    db = sqlite3.connect("gamers.db")
+
+#funactions
+def print_all_game():
+    '''print all the game nicely'''
+    db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     sql = "SELECT * from studio;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(results)
+    #loop through all the results
+    for studio in results:
+        print(f"{studio[1]:<40}{studio[2]:<6}")
     db.close() 
+
+
+
+
+
+#main code
+print_all_game()
